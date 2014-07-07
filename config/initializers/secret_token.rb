4eb66aee58f79ad3d769acda4fa90c775da60e1b
ken61502 <kennyson@gmail.com>
2014-07-07 12:32:03 +0800

    Init rails app and remote to heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WenPaper::Application.config.secret_key_base = 'dec61856b121696b04e7d0aeb62742acbcb05d60f49a6cf25c7374cc925742e31ee426cab9de3faff7d6ea5f80417fd980d365848d30bd1405326f7d50d61715'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WenPaper::Application.config.session_store :cookie_store, key: '_wen-paper_session'
